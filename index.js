449edc35f547c7240606ce95f056048830d47b35
Ifaz Howlader <ifaz.howl@gmail.com>
2025-07-28 23:27:39 -0400

    put more databse stuff now

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js"
 
-const firebaseConfig = {}
+const firebaseConfig = {
+    databaseURL: "https://leads-tracker-app-122b0-default-rtdb.firebaseio.com/",
+}
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+console.log(database)
   
 console.log(app)
 let inputEl = document.getElementById('input-el')
