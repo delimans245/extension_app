d2acebcec4ed593f75d884a4f0dc62840007df82
Ifaz Howlader <ifaz.howl@gmail.com>
2025-07-29 21:31:04 -0400

    deleting one more thing

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ console.log(app)
 let inputEl = document.getElementById('input-el')
 let saveEl = document.getElementById('save-el')
 let deleteEl = document.getElementById('delete-el')
-let tabEl = document.getElementById('tab-el')
 let listEl = document.getElementById('list-el')
 let links = []
 let localStorageEl = JSON.parse(localStorage.getItem('links'))
